45fefcc878348cb2b28aaee5faf038f807d13537
Bahman Baidar <bbaid001@wch136-22.cs.ucr.edu>
2021-02-13 18:19:44 -0800

    Part 2 first try

--- turnin/bbaid001_lab8_part1.c
+++ turnin/bbaid001_lab8_part1.c
@@ -7,10 +7,6 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
